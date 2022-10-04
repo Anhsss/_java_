4f6bcb23e6f0080b2bd2ae8173d4584769a20641
Anhsss <ghost00xdg7@naver.com>
2022-10-04 13:09:31 +0900

    중간테스트

--- 10.04/TestPersons.java
+++ 10.04/TestPersons.java
@@ -23,10 +23,10 @@ abstract class Person{
 	private int salary;
 	
 	public void show() {
-		System.out.println(super.toString() + "job = " + job +" "+ "dept = " + dept +" "+ "salary = " + salary);
+		System.out.println(super.toString() + " job = " + job +" "+ "dept = " + dept +" "+ "salary = " + salary);
 	}
 	public void increase() {
-		this.salary += 0.1 * salary;   // this.salary+(0.1*salary)
+		this.salary += (0.1 * salary);   // this.salary+(0.1*salary)
 	}
 	public Employee(String pnum, String name, int age, String job, String dept, int salary) {
 		super(pnum, name, age);
@@ -41,29 +41,29 @@ abstract class Person{
 	private int workYear;
 
 	public void show() {
-		System.out.println(super.toString() + "language = "+language+" "+"workYear = "+workYear);
+		System.out.println(super.toString() + " language = " + language + " workYear = " + workYear);
 	}
-	public void increase() {
-		this.workYear += workYear *0.1;
-	}
+//	public void increase() {
+//		this.workYear += (workYear *0.1);
+//	}
 	
 	public Designer(String pnum, String name, int age, String job, String dept, int salary, String language, int workYear) {
 		super(pnum, name, age, job, dept, salary);
 		this.language = language;
 		this.workYear = workYear;
 	}
-
+ }
 class Student extends Person{
 	private String schoolName;
 	private int schoolyear;
 	private int scholarship;
 
 	public void show() {
-		System.out.println(super.toString()+"schoolName = " + schoolName +" schoolyear = "+schoolyear +" scholarship = "+  scholarship);
+		System.out.println(super.toString() + " schoolName = " + schoolName + " schoolyear = "+schoolyear +" scholarship = "+  scholarship);
 	}
 	
 	public void increase() {
-		this.scholarship += scholarship * 0.1;
+		this.scholarship += (scholarship * 0.1);
 	}
 	
 	public Student(String pnum, String name, int age, String schoolName, int schoolyear, int scholarship ) {
@@ -78,10 +78,10 @@ class WorkStudent extends Student{
 	private int daypay;
 
 	public void show() {
-		System.out.println(super.toString() + "job = "+job+" daypay = "+daypay);
+		System.out.println(super.toString() + " Job = " + job + " daypay = " +daypay);
 	}
 	public void increase() {
-		this.daypay += daypay * 0.1;
+		this.daypay += (daypay * 0.1);
 	}
 	
 	public WorkStudent(String pnum, String name, int age, String schoolName, int schoolyear, int scholarship, String job, int daypay) {
@@ -94,17 +94,17 @@ class WorkStudent extends Student{
 
 public class TestPersons {
 	static void showAll(Person[] p) {
-		for(int i =0; i<p.length; i++) {
+		for(int i =0; i < p.length; i++) {
 			p[i].show();
 		}
 	}
-	static void increaseAll(Person[]p ) {
-		for(int i=0; i<p.length; i++) {
+	static void increaseAll(Person[] p) {
+		for(int i=0; i < p.length; i++) {
 			p[i].increase();
 		}
 	}
 
-	static void getData(Person[]p) {
+	static void getData(Person[] p) {
 
 		Employee e = new Employee("p01","Hong", 10, "DB", "Design", 2000);
 		
@@ -112,20 +112,20 @@ public class TestPersons {
 		
        	Student s = new Student("p03","Kim", 25, "html", 5000, 50);
         
-       	WorkStudent w = new WorkStudent("p04","Lee", 30, "pnu", 5, 500, "com", 1500);
+      	WorkStudent w = new WorkStudent("p04","Lee", 30, "pnu", 5, 500, "com", 1500);
 
 		p[0]= e;
 		p[1]= d;
 		p[2]= s;
 		p[3]= w;
-		}
+	}
 	public static void main(String[] args) {
 		Person [] setPerson = new Person[4];
 		getData(setPerson);
 		showAll(setPerson);
+		System.out.println("10% increase result :");
 		increaseAll(setPerson);
 		showAll(setPerson);
 	
-		}
 	}
 }
